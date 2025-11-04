70364e18231a0372d78ff83eba2ab54fcc3c4d0f
VaibhavGumalwad <121026410+VaibhavGumalwad@users.noreply.github.com>
2025-11-04 11:08:30 +0530

    keypair change

--- infra/main.tf
+++ infra/main.tf
@@ -101,7 +101,7 @@ resource "aws_instance" "app" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.sg.id]
   associate_public_ip_address = true
-  key_name                    = "my-ec2-deploy-key"
+  key_name                    = "keypair"
 
   tags = {
     Name = "nodejs-app-instance"
